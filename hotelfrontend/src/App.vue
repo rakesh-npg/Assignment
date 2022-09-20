da9660fb1d62f18aa0243cf51735fac462abc2dd
Rakesh <rakesh@ngpwebsmart.com>
2022-09-20 20:54:22 +0530

    test

---
+++ hotelfrontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <HotelTable/>
+    <CustomerTable/>
+    <v-main>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import HotelTable from './components/HotelTable.vue';
+import CustomerTable from './components/CustomerTable.vue';
+export default {
+  name: 'App',
+
+  components: {
+    HotelTable,
+    CustomerTable,
+},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
